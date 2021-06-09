6988678a72260a0eebcb52f854d4390a14f5d748
jim0409 <berserker.01.tw@hotmail.com>
2021-06-09 20:33:14 +0800

    update websocket client for multi-current

--- websocket/client.go
+++ websocket/client.go
@@ -1,29 +1,96 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gorilla/websocket"
 )
 
+var addr = "ws://127.0.0.1:8888/sockets/gxdash"
+
+func connect(addr string) (*websocket.Conn, error) {
+	c, _, err := websocket.DefaultDialer.Dial(addr, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+type Client interface {
+	Connect() error
+	ReadPump()
+}
+
+type wsClient struct {
+	Addr string
+	Conn *websocket.Conn
+}
+
+func (c *wsClient) ReadPump() {
+	conn := c.Conn
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			return
+		}
+		log.Printf("receive: %s\n", msg)
+	}
+
+}
+
+func (c *wsClient) Connect() error {
+	wsc, err := connect(c.Addr)
+	if err != nil {
+		return err
+	}
+	c.Conn = wsc
+	return nil
+}
+
+func newWsClient(addr string) Client {
+	return &wsClient{
+		Addr: addr,
+	}
+}
+
 func main() {
+	var wsClients []Client
 
-	c, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:3000/ws", nil)
-	if err != nil {
-		log.Fatal("dial:", err)
-	}
-	defer c.Close()
-
-	err = c.WriteMessage(websocket.TextMessage, []byte("hello message"))
-	if err != nil {
-		log.Println(err)
-		return
+	for i := 0; i < 10; i++ {
+		c := newWsClient(addr)
+		if err := c.Connect(); err != nil {
+			log.Fatal(err)
+		}
+		wsClients = append(wsClients, c)
 	}
 
-	_, msg, err := c.ReadMessage()
-	if err != nil {
-		log.Println("read:", err)
-		return
+	for _, j := range wsClients {
+		go j.ReadPump()
 	}
-	log.Printf("receive: %s\n", msg)
+
+	gracefulShutdown()
+
+}
+
+func gracefulShutdown() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
 }
