5e62ca37021f0d59cad099fb4940b984cb62d18d
jim0409 <berserker.01.tw@hotmail.com>
2021-06-28 02:12:22 +0800

    remove dangling code

--- networkServer/socket-server/tcp/TCP_customer_protocol/server_client_framework/server/router/router.go
+++ networkServer/socket-server/tcp/TCP_customer_protocol/server_client_framework/server/router/router.go
@@ -16,8 +16,7 @@ type Controller interface {
 	Excute(Msg) []byte
 }
 
-// 定義一個routers，並且在該router註冊處理方法
-var routers [][2]interface{}
+var routers = make([][2]interface{}, 0)
 
 // Router : 透過解析pred的類別，來將controller正確定義到 `routers` 上
 func Route(pred interface{}, controller Controller) {
@@ -49,18 +48,15 @@ func Route(pred interface{}, controller Controller) {
 			}
 			return true
 		}
-		var arr [2]interface{}
-		// 將defaultPred存入arr[0]
-		arr[0] = defaultPred
-		arr[1] = controller
-		routers = append(routers, arr)
+
+		routers = append(routers, [2]interface{}{defaultPred, controller})
+
 		fmt.Println(routers)
 	default:
 		fmt.Println("No match requested controller")
 	}
 }
 
-// TaskDeliver 會輸入預計接收資料的bufferBytes以及conn(連線)
 func TaskDeliver(postdata []byte, conn net.Conn) {
 	for _, v := range routers {
 		pred := v[0]
@@ -71,23 +67,19 @@ func TaskDeliver(postdata []byte, conn net.Conn) {
 			log.Println(err)
 		}
 
-		// pred這邊定義的函數為來自router的Controller，只有當Controller回傳為true的時候才進行寫入
 		if pred.(func(entermsg Msg) bool)(entermsg) {
 			result := act.(Controller).Excute(entermsg)
 
-			// 把結果(result)寫回conn，期許client端也會收到對應的資訊
 			conn.Write(result)
 			return
-		} else {
-			conn.Write([]byte("Hey! The meta data is invalid!"))
-			return
 		}
+
+		conn.Write([]byte("Hey! The meta data is invalid!"))
+		return
 	}
 }
 
-// EchoController 為一個controller的實例，所有controller都需要在init()時註冊到router，才能被router所分配
-type EchoController struct {
-}
+type EchoController struct{}
 
 func (e *EchoController) Excute(m Msg) []byte {
 	log.Println("Receive the msg ", m)
@@ -101,20 +93,11 @@ func (e *EchoController) Excute(m Msg) []byte {
 	return msg
 }
 
-// 透過初始化設定，將已定義的Execute方法的`資料結構`註冊到router
 func init() {
 	var echo EchoController
 
-	// 實例routers
-	routers = make([][2]interface{}, 0, 20)
-
 	// Route註冊echo這個資料結構到router上
 	Route(func(entry Msg) bool {
-		// 如果entry.Meta的值為"test"則返回true
-		// if entry.Meta["meta"] == "pass" {
-		// 	return true
-		// }
-		// return false
 		return entry.Meta["meta"] == "pass"
 	}, &echo)
 }
