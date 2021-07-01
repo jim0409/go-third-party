9830477cb49808699bde82896f7550bcc4d612f3
jim0409 <berserker.01.tw@hotmail.com>
2021-07-02 03:26:19 +0800

    update consumer module

--- sarama_kafka/practice/consumer.go
+++ sarama_kafka/practice/consumer.go
@@ -3,28 +3,24 @@ package main
 import (
 	"context"
 	"log"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
 
 	"github.com/Shopify/sarama"
 )
 
 type Consumer struct {
-	ready chan bool
+	Topics        []string
+	ConsumerGroup sarama.ConsumerGroup
 }
 
-func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
-	close(consumer.ready)
+func (c *Consumer) Setup(sarama.ConsumerGroupSession) error {
 	return nil
 }
 
-func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+func (c *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
 	return nil
 }
 
-func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+func (c *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for message := range claim.Messages() {
 		log.Printf("Message claimed: key = %s, value = %v, topic = %s, partition = %v, offset = %v", string(message.Key), string(message.Value), message.Topic, message.Partition, message.Offset)
 		session.MarkMessage(message, "")
@@ -33,57 +29,53 @@ func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, clai
 	return nil
 }
 
-func NewKafkaConsumer(brokers []string, group string) sarama.ConsumerGroup {
+func (c *Consumer) Execute(topics []string) error {
+	// var err error
+	var echan = make(chan error, 1)
+	go func() {
+		for {
+			if err := c.ConsumerGroup.Consume(context.TODO(), topics, c); err != nil {
+				// log.Panicf("Error from consumer: %v", err)
+				echan <- err
+				return
+			}
+		}
+	}()
+
+	return <-echan
+}
+
+type ConsumerImp interface {
+	// must satisfy the ConsumerGroupHandle interface {Setup, Cleanup, ConsumeClaim}
+	Setup(sarama.ConsumerGroupSession) error
+	Cleanup(sarama.ConsumerGroupSession) error
+	ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error
+
+	// start loop consume msg via consumer group
+	Execute([]string) error
+}
+
+func NewConsumeHandler(topics []string, brokers []string, group string) ConsumerImp {
 	config := sarama.NewConfig()
 	client, err := sarama.NewConsumerGroup(brokers, group, config)
 	if err != nil {
 		log.Panicf("Error creating consumer group client: %v", err)
 	}
 
-	return client
+	return &Consumer{
+		Topics:        topics,
+		ConsumerGroup: client,
+	}
 }
 
 func main() {
+	topics := []string{"sarama"}
 	broker := "127.0.0.1:9092"
 	group := "test"
-	topics := []string{"sarama"}
 
-	consumer := Consumer{
-		ready: make(chan bool, 0),
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	client := NewKafkaConsumer([]string{broker}, group)
-
-	wg := &sync.WaitGroup{}
-	go func() {
-		wg.Add(1)
-		defer wg.Done()
-		for {
-			if err := client.Consume(ctx, topics, &consumer); err != nil {
-				log.Panicf("Error from consumer: %v", err)
-			}
-			if ctx.Err() != nil {
-				return
-			}
-			consumer.ready = make(chan bool, 0)
-		}
-	}()
-
-	<-consumer.ready
-
-	sigterm := make(chan os.Signal, 1)
-	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
-	select {
-	case <-ctx.Done():
-		log.Println("terminating: context cancelled")
-	case <-sigterm:
-		log.Println("terminating: via signal")
-	}
-	cancel()
-	wg.Wait()
-	if err := client.Close(); err != nil {
-		log.Panicf("Error closing client: %v", err)
+	// declare the consumer handler
+	consumer := NewConsumeHandler(topics, []string{broker}, group)
+	if err := consumer.Execute(topics); err != nil {
+		log.Fatal(err)
 	}
 }
