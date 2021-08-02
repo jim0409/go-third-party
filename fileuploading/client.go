8e2d66bbeb835ee23048bb266455577235d6b302
jim0409 <berserker.01.tw@hotmail.com>
2021-08-02 15:48:38 +0800

    add client.go for testing

---
+++ fileuploading/client.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+)
+
+type UserInfo struct {
+	Id         string
+	Name       string
+	CustomerId string
+	Key        string
+	Sec        string
+}
+
+// test for reading csv file
+func readCsvFile(f string) []UserInfo {
+	csvfile, err := os.Open(f) // Open the file
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(csvfile) // or ... r := csv.NewReader(bufio.NewReader(csvfile))
+
+	UserInfos := make([]UserInfo, 0) // Read each record from csv
+	for {
+		record, err := r.Read() // Read each record from csv
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		u := UserInfo{
+			Id:         record[0],
+			Name:       record[1],
+			CustomerId: record[2],
+			Key:        record[3],
+			Sec:        record[4],
+		}
+		UserInfos = append(UserInfos, u)
+	}
+	return UserInfos
+}
+
+func main() {
+	readCsvFile("files/upload.csv")
+}
