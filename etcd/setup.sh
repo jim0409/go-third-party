76a12a653f9b1d9854545266f41a93dd77b6ffa1
jim0409 <berserker.01.tw@hotmail.com>
2021-08-18 14:36:04 +0800

    update etcd env

--- etcd/setup.sh
+++ etcd/setup.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
+dirname=$PWD
+SERVICE_NAME="morse-e3w"
+IMAGE_TAG=`git rev-parse --short=6 HEAD`        # retrive tag to specified git tag
+IMAGE_PREFIX="reg.paradise-soft.com.tw:5000/"   # this vary with different project name
+IMAGE_NAME="$IMAGE_PREFIX$SERVICE_NAME:latest"
 
-# dig k8s ip and cat and config.go file
+HELP_DOC="
+    >$ ./helper.sh [param]
+    (
+      param:
+        <build>     : build docker image with name 'project_name/folder_name:git_commit_tag'.
+        <push>      : push docker images 'project_name/folder_name:git_commit_tag' to registry.
+    )
+"
 
-ip1=`dig etcd1.dev-morse.svc.cluster.local +short`
-ip2=`dig etcd2.dev-morse.svc.cluster.local +short`
-ip3=`dig etcd3.dev-morse.svc.cluster.local +short`
+function docker_build() {
+    docker build --build-arg gitTag=$IMAGE_TAG -t $IMAGE_NAME .
+    if [ $? != 0 ];then
+        echo "fail to build..."
+        exit 1
+    fi
+    echo 'build success'
+}
+
+function docker_push() {
+    docker push $IMAGE_NAME
+    if [ $? != 0 ];then
+        echo "fail to push..."
+        exit 1
+    fi
+    echo 'push success'
+}
+
+function help() {
+cat << HELP 
+    $HELP_DOC 
+HELP
+}
 
 
-echo "$ip1:2380,$ip2:2380,$ip3:2380"
+
+if [ "$1" == "build" ]; then
+    docker_build
+elif 
+    [ "$1" == "push" ]; then
+    docker_push
+else
+    help
+fi
