cc5a18c571b88ea3dc5500e6b22e81a4434a10dc
jim0409 <berserker.01.tw@hotmail.com>
2021-08-23 00:40:56 +0800

    update

--- file_upload/main.go
+++ file_upload/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-// func uploadFile(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Uploading File")
-// }
+func ShowCSVRecord(file io.Reader) {
+	r := csv.NewReader(file)
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(record)
+	}
+}
+
+func BackUpFile(file io.Reader) {
+	tempFile, err := ioutil.TempFile("files", "upload-*.backup") // `*` 會隨機產生一個亂序 id
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileBytes, err := ioutil.ReadAll(file)
+	tempFile.Write(fileBytes)
+}
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method)
@@ -30,17 +58,12 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Uploaded File: %+v\n", handler.Filename)
 		fmt.Printf("File Size: %+v\n", handler.Size)
 		fmt.Printf("MIME Header: %+v\n", handler.Header)
+
 		// 3. write temporary file on our server
-		tempFile, err := ioutil.TempFile("files", "upload-*.csv")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer tempFile.Close()
-		fileBytes, err := ioutil.ReadAll(file)
-		if err != nil {
-			fmt.Println(err)
-		}
-		tempFile.Write(fileBytes)
+		// 因為 io.Reader 讀取完畢就會回收。所以一次只能開啟一個，不然就是要另外做 bytes.Buffer
+		ShowCSVRecord(file)
+		// BackUpFile(file)
+
 		// 4. return result
 		fmt.Fprintf(w, "Successfully Uploaded File\n")
 	}
@@ -48,8 +71,9 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 
 func setupRoutes() {
 	http.HandleFunc("/upload", uploadFile)
-	http.ListenAndServe(":2020", nil)
+	http.ListenAndServe(":8000", nil)
 }
+
 func main() {
 	setupRoutes()
 }
