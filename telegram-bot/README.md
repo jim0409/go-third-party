ab1587a7812e60dc2ec3e6799997bed16aae5792
jim0409 <berkser.01.tw@hotmail.com>
2021-12-14 15:37:08 +0800

    misc: add telegram bot

---
+++ telegram-bot/README.md
@@ -0,0 +1,3 @@
+# refer:
+- https://zaoldyeck.medium.com/%E6%89%8B%E6%8A%8A%E6%89%8B%E6%95%99%E4%BD%A0%E6%80%8E%E9%BA%BC%E6%89%93%E9%80%A0-telegram-bot-a7b539c3402a
+- https://medium.com/@ManHay_Hong/how-to-create-a-telegram-bot-and-send-messages-with-python-4cf314d9fa3e
---
+++ telegram-bot/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+/*
+- https://api.telegram.org/bot5020139909:AAGJlFRhCGnr6PpiHjHg23sZWUUSEnX2ZZs/getUpdates
+> 拿到所有訊息
+
+- https://api.telegram.org/bot5020139909:AAGJlFRhCGnr6PpiHjHg23sZWUUSEnX2ZZs/sendMessage?chat_id=1068429272&parse_mode=Markdown&text=“watch you slowly bb”
+> 發送訊息
+
+*/
+
+func main() {
+	resp, err := http.Get("https://api.telegram.org/bot5020139909:AAGJlFRhCGnr6PpiHjHg23sZWUUSEnX2ZZs/sendMessage?chat_id=1068429272&parse_mode=Markdown&text=“watch out")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+}
