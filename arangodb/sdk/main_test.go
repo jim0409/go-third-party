04e97ca6d82e82e62e681014808c58e638710a8a
jim0409 <berkser.01.tw@hotmail.com>
2022-02-24 13:07:24 +0800

    misc: update

--- arangodb/sdk/main_test.go
+++ arangodb/sdk/main_test.go
@@ -38,3 +38,62 @@ func QueryInsertCollection(col string, db driver.Database) error {
 
 	return nil
 }
+
+func QueryCount(db driver.Database, col string) (int64, error) {
+	ctx := context.Background()
+	collection, err := db.Collection(ctx, col)
+	if err != nil {
+		return 0, err
+	}
+	count, err := collection.Count(ctx)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+func TestQueryCount(t *testing.T) {
+	db, err := NewArangoDB("http://127.0.0.1:8529")
+	assert.Nil(t, err)
+	count, err := QueryCount(db, "AdminAccounts")
+	assert.Nil(t, err)
+	fmt.Println(count)
+}
+
+// https://www.arangodb.com/docs/stable/drivers/go-example-requests.html#querying-documents-one-document-at-a-time
+// QueryOrm : arangodb 查詢使用 ORM，需要使用指定的 context，或者透過 driver.IsNoMoreDocuments 來撈取資料
+func QueryOrm(db driver.Database, rawQueryStr string, bindVars map[string]interface{}) (*[]interface{}, error) {
+	// ctx := driver.WithQueryCount(context.Background())
+	ctx := context.Background()
+	cursor, err := db.Query(ctx, rawQueryStr, bindVars)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close()
+
+	fmt.Printf("query yields %d\n", cursor.Count())
+	for {
+		var doc interface{}
+		meta, err := cursor.ReadDocument(ctx, &doc)
+		if driver.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+		fmt.Println("---------", meta)
+		fmt.Println("=========", doc)
+	}
+
+	return nil, nil
+}
+
+func TestQueryOrm(t *testing.T) {
+	db, err := NewArangoDB("http://127.0.0.1:8529")
+	assert.Nil(t, err)
+
+	qstr := `FOR d IN AdminAccounts RETURN d`
+	data, err := QueryOrm(db, qstr, nil)
+	assert.Nil(t, err)
+
+	fmt.Println(data)
+}
