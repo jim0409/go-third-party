24c3f23286913103de7f2be26f4eea7eabb1fa0f
jim0409 <berkser.01.tw@hotmail.com>
2022-04-01 18:21:43 +0800

    misc: download xlsx example

---
+++ xlsx/README.md
@@ -0,0 +1,3 @@
+# refer:
+- https://medium.com/@sebastian_4031/serving-xlsx-file-from-golang-web-application-7eb6965ee7aa
+- https://github.com/tealeg/xlsx/blob/master/tutorial/tutorial.adoc
--- xlsx/main.go
+++ xlsx/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/tealeg/xlsx/v3"
 )
 
@@ -20,4 +24,40 @@ func main() {
 	}
 	fmt.Println("----")
 
+	router := gin.New()
+	router.LoadHTMLGlob("templates/*")
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	router.GET("/", homeHandler)
+	router.GET("/excel-download", serveExcel)
+
+	router.Run(":3000")
+}
+
+func homeHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "home.tmpl", nil)
+}
+
+func serveExcel(c *gin.Context) {
+	file := xlsx.NewFile()
+	for i := 0; i < 10; i++ {
+		sheet, err := file.AddSheet(fmt.Sprintf("sheet-%d", i))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+		row := sheet.AddRow()
+		cell := row.AddCell()
+		cell.Value = "I am a cell!"
+	}
+	var b bytes.Buffer
+	if err := file.Write(&b); err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	downloadName := time.Now().UTC().Format("data-20060102150405.xlsx")
+	c.Header("Content-Description", "File Transfer")
+	c.Header("Content-Disposition", "attachment; filename="+downloadName)
+	c.Data(http.StatusOK, "application/octet-stream", b.Bytes())
 }
