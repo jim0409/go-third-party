0afff893a7bab1780bb7da510610c30464fc2797
jim0409 <berkser.01.tw@hotmail.com>
2022-05-16 12:36:19 +0800

    misc: add extra operator for bytes buffer

--- networkServer/socket-server/tcp/TCP_customer_protocol/bytesBuffer/packets_test.go
+++ networkServer/socket-server/tcp/TCP_customer_protocol/bytesBuffer/packets_test.go
@@ -9,11 +9,21 @@ import (
 
 func TestEnpack(t *testing.T) {
 	bytesBuffer := &bytes.Buffer{}
+	// for each byte is uint8
+	// ,meanwhile its only 256 for each value!
 	array := make([]byte, 20)
 	array[0] = byte(1)
 	// array[1] = byte(2 >> 8)
 	array[1] = byte(2)
-	array[2] = byte(33332 >> 8)
+	array[2] = byte(33332 >> 8) // divide 2^8
+	array[3] = byte(10 << 2)    // multiple 2^2
+	array[4] = byte(255)        // max number of each element
+	array[5] = byte(0xff)       // replace number with 16 digits
+	array[6] = byte(1 & 0xff)   // and logic with 0xff
+	array[7] = byte(8 & 0xff)   // and with numeric number
+	array[8] = byte(16 & 0xff)  //  ...
+	array[9] = byte(124 | 0xff) // or with 0xff
+	array[10] = byte(1 ^ 0xff)  // orNot with 0xff
 	// bytesBuffer.WriteString("a" >> 8)
 	// bytesBuffer.WriteRune(array)
 	for _, j := range array {
