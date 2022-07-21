06da8a9c700e309336bd0d1859b4e2f0ba8dcae9
jim0409 <berserker.01.tw@hotmail.com>
2022-07-22 07:15:47 +0800

    misc: update finite state machine example

--- finite-state-machine/simple2/main_test.go
+++ finite-state-machine/simple2/main_test.go
@@ -10,6 +10,7 @@ const (
 	Logout = "LOGOUT"
 	Login  = "LOGIN"
 	Start  = "START"
+	Play   = "PLAY"
 	Pause  = "PAUSE"
 	End    = "END"
 )
@@ -17,21 +18,22 @@ const (
 func TestStateMachine(t *testing.T) {
 	fm := &Machine{
 		Handlers:   make(map[string]Handler),
-		StartState: Logout,
+		StartState: Login,
 		EndStates:  make(map[string]bool),
 	}
 
 	// ad state
-	fm.AddState(Logout, LoginHandler)
-	fm.AddState(Login, StartHandler)
-	fm.AddState(Start, PauseHandler)
-	fm.AddState(Pause, EndHandler)
-	fm.AddState(End, LogoutHandler)
+	fm.AddState(Login, LoginHandler)
+	fm.AddState(Start, StartHandler)
+	fm.AddState(Play, PlayHandler)
+	fm.AddState(Pause, PauseHandler)
+	fm.AddState(End, EndHandler)
+	fm.AddState(Logout, LogoutHandler)
 
 	// add end state
 	fm.AddEndState(Logout)
 
-	fm.Execute(Cargo)
+	fm.Execute(Play)
 	fmt.Println("====== finished =======")
 
 }
@@ -41,27 +43,38 @@ func Cargo() interface{} {
 }
 
 func LoginHandler(i interface{}) (string, interface{}) {
-	fmt.Println("execute login")
-	return Login, StartHandler
+	fmt.Println("execute login with cargo status", i)
+	return Start, StartHandler
 }
 
 func StartHandler(i interface{}) (string, interface{}) {
-	fmt.Println("execute start")
-	return Start, PauseHandler
+	fmt.Println("execute start with cargo status", i)
+	return Play, PlayHandler
+}
+
+var count = 0
+
+func PlayHandler(i interface{}) (string, interface{}) {
+	fmt.Println("execute play with cargo status", i, count)
+	count = count + 1
+	if count < 3 {
+		return Play, PlayHandler
+	}
+	return Pause, PauseHandler
 }
 
 func PauseHandler(i interface{}) (string, interface{}) {
-	fmt.Println("execute pause")
-	return Pause, EndHandler
+	fmt.Println("execute pause with cargo status", i)
+	return End, EndHandler
 }
 
 func EndHandler(i interface{}) (string, interface{}) {
-	fmt.Println("execute end")
-	return End, LogoutHandler
+	fmt.Println("execute end with cargo status", i)
+	return Logout, LogoutHandler
 }
 
 func LogoutHandler(i interface{}) (string, interface{}) {
-	fmt.Println("execute logout")
+	fmt.Println("execute logout with cargo status", i)
 	return Logout, nil
 }
 
