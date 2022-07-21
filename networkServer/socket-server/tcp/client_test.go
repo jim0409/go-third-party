992eb055febca1977dda949bc5edf3b5fafe5afd
jim0409 <berkser.01.tw@hotmail.com>
2022-07-21 16:06:19 +0800

    fix: fix tcp client

--- networkServer/socket-server/tcp/client_test.go
+++ networkServer/socket-server/tcp/client_test.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"testing"
 )
 
-func sender(conn net.Conn) {
-	words := "hello world!"
-	conn.Write([]byte(words))
-	fmt.Println("send over")
+const server = "127.0.0.1:1024"
 
+type Client struct {
+	Conn *net.TCPConn
 }
 
-func main() {
-	server := "127.0.0.1:1024"
+func NewTcpClient() *Client {
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", server)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
@@ -27,7 +26,20 @@ func main() {
 		os.Exit(1)
 	}
 
+	return &Client{
+		Conn: conn,
+	}
+}
+
+func (c *Client) Send(bs []byte) {
+	c.Conn.Write(bs)
+	fmt.Println("send over")
+}
+
+func TestSendRawMsg(t *testing.T) {
+	client := NewTcpClient()
+	client.Send([]byte("hello world!"))
+
 	fmt.Println("connect success")
-	sender(conn)
 
 }
