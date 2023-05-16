34b7d384f8bf13ae7fea3937148446cf8c4684e8
jim.weng <berserker.01.tw@hotmail.com>
2023-05-16 18:02:31 +0800

    misc: add go-builtin sort.Slice

---
+++ sort/README.md
@@ -0,0 +1,11 @@
+# intro
+
+透過 golang 內建的 sort 包, 排序切片
+
+note: 只能在 go 1.8 以後的版本使用
+ 
+
+# refer:
+- https://stackoverflow.com/questions/37695209/golang-sort-slice-ascending-or-descending
+- https://books.studygolang.com/Mastering_Go_ZH_CN/eBook/chapter3/03.3.7.html
+- https://calvertyang.github.io/2021/09/16/golang-sorting-and-custom-sorting-by-functions/
---
+++ sort/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	// Sorting a slice of strings by length
+	strs := []string{"United States", "India", "France", "United Kingdom", "Spain"}
+	sort.Slice(strs, func(i, j int) bool {
+		return len(strs[i]) < len(strs[j])
+	})
+	fmt.Println("Sorted strings by length: ", strs)
+
+	// Stable sort
+	sort.SliceStable(strs, func(i, j int) bool {
+		return len(strs[i]) < len(strs[j])
+	})
+	fmt.Println("[Stable] Sorted strings by length: ", strs)
+
+	// Sorting a slice of strings in the reverse order of length
+	sort.SliceStable(strs, func(i, j int) bool {
+		return len(strs[j]) < len(strs[i])
+	})
+	fmt.Println("[Stable] Sorted strings by reverse order of length: ", strs)
+
+	a := []int{5, 3, 4, 7, 8, 9}
+	sort.Slice(a, func(i, j int) bool {
+		return a[i] < a[j]
+	})
+	for _, v := range a {
+		fmt.Println(v)
+	}
+}
